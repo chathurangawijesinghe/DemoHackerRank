1a681c237c7f892be95e5c915f8c90a14b3cb43d
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-02-06 11:07:18 +0800

    remove ignored files

---
+++ DemoHackerRank/Kangaroo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoHackerRank
+{
+    public class Kangaroo
+    {
+        // Complete the kangaroo function below.
+        public static string kangaroo(int x1, int v1, int x2, int v2)
+        {
+            if (x1 == x2 && v1 == v2)
+            {
+                return "YES";
+            }
+            if ((x1 > x2 && v1 > v2) || (x2 > x1 && v2 > v1))
+            {
+                return "NO";
+            }
+
+            if (x1 > x2)
+            {
+                return CanMeet(x1, v1, x2, v2);
+            }
+            else
+            {
+                return CanMeet(x2, v2, x1, v1);
+            }
+        }
+
+        static string CanMeet(int high, int d1, int low, int d2)
+        {
+            while (high != low)
+            {
+                high += d1;
+                low += d2;
+
+                if (high < low)
+                {
+                    break;
+                }
+            }
+
+            if (high == low)
+            {
+                return "YES";
+            }
+            else
+            {
+                return "NO";
+            }
+        }
+    }
+}
---
+++ DemoHackerRank/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+
+namespace DemoHackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CountApplesAndOranges.countApplesAndOranges(7, 11, 5, 15, new int[] { -2, 2, 1 }, new int[] { 5, -6 });
+            //CountingValleys.countingValleys(8, "UDDDUDUU");
+            //JumpingOnTheClouds.jumpingOnClouds(new int[] { 0, 0, 1, 0, 0, 1, 0 });
+            //RepeatedString.repeatedString("abc", 1000000000000);
+            //Arrays.hourglassSum(new int[][] {
+            //    new int[] { 1,1,1,0,0,0 },
+            //    new int[] { 0,1,0,0,0,0 },
+            //    new int []{ 1,1,1,0,0,0 },
+            //    new int []{ 0,0,2,4,4,0 },
+            //    new int []{0,0,0,2,0,0 },
+            //    new int []{ 0,0,1,2,4,0 }
+            //});
+            //Arrays.rotLeft(new int[] { 1,2,3,4,5}, 4);
+
+            Arrays.arrayManipulation(10, new int[][] { 
+                new int[] { 1, 5, 3 }, 
+                new int[] { 4, 8, 7 },
+                new int[] { 6, 9, 1 }
+            });
+            Console.WriteLine("Hello World!");
+        }
+
+        // Complete the sockMerchant function below.
+        static int sockMerchant(int n, int[] ar)
+        {
+            Stack<int> stack = new Stack<int>();
+            Array.Sort(ar);
+
+            int pairs = 0;
+
+            foreach (var a in ar)
+            {
+                if (stack.Contains(a))
+                {
+                    stack.Pop();
+                    pairs++;
+                }
+                else
+                {
+                    stack.Push(a);
+                }
+            }
+
+            return pairs;
+        }
+    }
+}
