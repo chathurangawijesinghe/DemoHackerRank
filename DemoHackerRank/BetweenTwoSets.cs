3819f0b69e4c2d28dcf51e979b6fc8a9dae8d702
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-04-30 00:07:30 +0800

    all

---
+++ DemoHackerRank/BetweenTwoSets.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoHackerRank
+{
+    public class BetweenTwoSets
+    {
+        /*
+     * Complete the 'getTotalX' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+        //public static int getTotalX(List<int> a, List<int> b)
+        //{
+
+        //}
+    }
+}
---
+++ DemoHackerRank/MinimumDistances.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoHackerRank
+{
+    class MinimumDistances
+    {
+        // Complete the minimumDistances function below.
+        public static int minimumDistances(int[] a)
+        {
+            List<int> list = new List<int>();
+            for (var i = 0; i < a.Length; i++)
+            {
+                var index = Array.LastIndexOf(a, a[i]);
+                if (index > 0 && index > i)
+                {
+                    list.Add(index - i);
+                }
+            }
+
+            if (list.Count() > 0)
+            {
+                return list.Min();
+            }
+
+            return -1;
+        }
+    }
+}
